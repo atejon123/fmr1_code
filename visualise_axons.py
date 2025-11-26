93eeeaee867d549ae868f63e4251177916d0c14a
atejon123 <45932034+atejon123@users.noreply.github.com>
2025-11-26 15:59:09 +0000

    axon visualisation file added

---
+++ visualise_axons.py
@@ -0,0 +1,26 @@
+
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+import glob
+
+import os
+
+
+from brainrender import Scene , actors, settings, actor, camera, cameras
+from brainrender.actors import Neuron, Points, Point
+
+
+settings.SHADER_STYLE = "ambient"
+settings.SHOW_AXES = False
+
+scene = Scene(title="", atlas_name='allen_mouse_25um', check_latest=False)
+
+coords_file = pd.read_csv("inputs/167_mch_compiled_shifted_cells.csv")
+
+coords = coords_file.to_numpy()
+
+scene.add(Points(coords, radius = 20, colors="black", alpha=1))
+
+scene.export("outputs/interactive_167_mch.html")
