7bde231a6644fe04fba34228a0548401ad7bfbf2
atejon123 <45932034+atejon123@users.noreply.github.com>
2025-11-26 17:10:38 +0000

    update

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -cwd -pe sharedmem 8 -l,h_vmem=19G,h_rt=47:59:59, -M ajonausk@ed.ac.uk -m e
+#$ -cwd -pe sharedmem 8 -l h_vmem=19G,h_rt=47:59:59 -M ajonausk@ed.ac.uk -m e
 
 source $HOME/.bashrc
 cd /exports/eddie/scratch/ajonausk/fmr1_code
